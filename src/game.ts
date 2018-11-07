fdcccb9089601a05592e962befe95bee3a321f01
Sam Barnes <thbarnes@centene.com>
2018-11-07 16:32:00 -0600

    added code to pass tests for high card and two of a kind

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,6 @@
+const SUITS = ['C', 'H', 'S', 'D'];
+const RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
 export class Game {
   hand
   constructor(private cards) {
@@ -5,15 +8,14 @@ export class Game {
   }
 
   bestHand() {
-
+    const evaluator = new HandEvaluator(this.hand);
+    return evaluator.evaluate();
   }
 }
 
 class Card {
   rank: string;
   suit: string;
-  suits = ['C', 'H', 'S', 'D'];
-  ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
   constructor(private cardString: string) {
     this.rank = cardString.slice(0, cardString.length - 1);
@@ -22,7 +24,7 @@ class Card {
   }
 
   validate() {
-    if (this.suits.includes(this.suit) && this.ranks.includes(this.rank)) {
+    if (SUITS.includes(this.suit) && RANKS.includes(this.rank)) {
       return;
     }
     throw new Error('Invalid card');
@@ -54,5 +56,44 @@ class Hand {
     if (new Set(this.hand.map(card => card.toString())).size !== this.handSize) {
       throw new Error('Duplicate cards');
     }
+  }
+}
+
+class HandEvaluator {
+  constructor(private hand) { }
+
+  evaluate() {
+    return this.twoOfAKind() || this.highCard();
+  }
+
+  private highCard() {
+    const highCardRank = this.hand.hand.reduce((acc, cur) => {
+      const rank = cur.rank;
+      return RANKS.indexOf(rank) > RANKS.indexOf(acc) ? rank : acc;
+    }, '2');
+
+    return `High Card (${highCardRank} high)`;
+  }
+
+  private twoOfAKind() {
+    const rankCounts = this.hand.hand.reduce((acc, cur) => {
+      const rank = cur.rank;
+      if (acc[rank])
+        return ({
+          ...acc,
+          [rank]: ++acc[rank]
+        });
+      return ({
+        ...acc,
+        [rank]: 1,
+      });
+    }, {});
+    const pairs = Object.keys(rankCounts)
+      .map(rank => ({ rank, count: rankCounts[rank] }))
+      .filter(c => c.count === 2);
+    if (pairs.length === 1)
+      return `Two of a Kind (${pairs[0].rank} high)`;
+
+    return false;
   }
 }
